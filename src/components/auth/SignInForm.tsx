78f7090e80cf4c6da22dbb935932fb3d43a5f396
kukarachass <maksimvozniy18@gmail.com>
2026-05-21 20:11:33 +0200

    sign-in & sign-out methods added

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,10 +1,46 @@
+"use client"
+
 import SocialsMethod from "@/components/auth/SocialsMethod";
 import FormInput from "@/components/auth/FormInput";
 import PasswordInput from "@/components/auth/PasswordInput";
 import ButtonPrimary from "@/components/ui/buttons/ButtonPrimary";
 import Link from "next/link";
+import {useForm} from "react-hook-form";
+import {SignInFormValues, signInSchema} from "@/lib/validators/sign-in-schema";
+import getSafeCallbackUrl from "@/utils/get-safe-callback";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {toast} from "sonner";
+import {useRouter} from "next/navigation";
+import {authClient} from "@/lib/auth-client";
 
 export default function SignInForm(){
+    const callbackUrl = getSafeCallbackUrl("/");
+    const router = useRouter();
+    const { signIn } = authClient;
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<SignInFormValues>({
+        resolver: zodResolver(signInSchema),
+        mode: "onTouched", // Ошибки появятся, когда пользователь "потрогает" поле и уйдет с него
+    });
+
+    async function onSubmit(data: SignInFormValues) {
+        const res = await signIn.email({
+            ...data,
+            callbackURL: callbackUrl,
+        });
+
+        if (res.error) {
+            toast.error(res.error.message || "Something went wrong.");
+        } else {
+            router.push(callbackUrl) // теперь безопасно
+            toast.success("Sign In successfully");
+        }
+    }
+
     return(
         <div className="flex flex-col gap-4 w-full">
             <div className="flex flex-col gap-4 items-center pb-8">
@@ -17,11 +53,22 @@ export default function SignInForm(){
                 <span className="text-[var(--text)] text-[16px]">or</span>
                 <div className="h-[1px] bg-gray-200 w-full"/>
             </div>
-            <form className="flex flex-col gap-4" action="">
-                <FormInput placeholder="Email" name="email" type={"email"}/>
-                <PasswordInput placeholder="Password" name="password" type={"password"}/>
-                <ButtonPrimary variant={"primary"}>
-                    Sign In
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-2">
+                    <FormInput {...register("email")} placeholder="Email" name="email" type={"email"}/>
+                    {errors.email && <p className="text-[13px] text-red-500 font-[600]">{errors.email.message}</p>}
+                </div>
+
+                <div className="flex flex-col gap-2">
+                    <PasswordInput {...register("password")} placeholder="Password" name="password" type={"password"}/>
+                    {errors.password && <p className="text-[13px] text-red-500 font-[600]">{errors.password.message}</p>}
+                </div>
+
+                <ButtonPrimary
+                    disabled={isSubmitting}
+                    variant={"primary"}
+                >
+                    { isSubmitting ? "Signing in..." : "Sign in"}
                 </ButtonPrimary>
                 <Link className="text-[14px] text-[var(--muted)] underline text-center" href="#">Forgot Password?</Link>
             </form>
--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,10 +1,51 @@
+"use client"
+
 import SocialsMethod from "@/components/auth/SocialsMethod";
 import FormInput from "@/components/auth/FormInput";
 import PasswordInput from "@/components/auth/PasswordInput";
 import ButtonPrimary from "@/components/ui/buttons/ButtonPrimary";
 import Link from "next/link";
+import getSafeCallbackUrl from "@/utils/get-safe-callback";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {SignUpFormValues, signUpSchema} from "@/lib/validators/sign-up-schema";
+import {authClient} from "@/lib/auth-client";
+import {toast} from "sonner";
+import {useRouter} from "next/navigation";
 
 export default function SignUpForm(){
+    const router = useRouter();
+    const callbackUrl = getSafeCallbackUrl("/");
+    const { signUp } = authClient;
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isSubmitting},
+    } = useForm<SignUpFormValues>({
+        resolver: zodResolver(signUpSchema),
+        mode: "onTouched"
+    });
+
+    async function onSubmit(data: SignUpFormValues) {
+        const {error} = await signUp.email({
+            email: data.email,
+            password: data.password,
+            name: data.name,
+        });
+
+        if (error) {
+            toast.error(error.message || "Something went wrong.");
+        } else {
+            toast.success("Sign up successfully");
+
+            if (callbackUrl === "/") {
+                router.push("/");
+            } else {
+                router.push(callbackUrl);
+            }
+        }
+    }
+
     return(
         <div className="flex flex-col gap-4 w-full">
             <div className="flex flex-col gap-4 items-center pb-8">
@@ -17,12 +58,24 @@ export default function SignUpForm(){
                 <span className="text-[var(--text)] text-[16px]">or</span>
                 <div className="h-[1px] bg-gray-200 w-full"/>
             </div>
-            <form className="flex flex-col gap-4" action="">
-                <FormInput placeholder="Name" name="name" type={"text"}/>
-                <FormInput placeholder="Email" name="email" type={"email"}/>
-                <PasswordInput placeholder="Password" name="password" type={"password"}/>
-                <ButtonPrimary variant={"primary"}>
-                    Sign Up
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-2">
+                    <FormInput {...register("name")} placeholder="Name" name="name" type={"text"}/>
+                    {errors.name && <p className="text-[13px] text-red-500 font-[600]">{errors.name.message}</p>}
+                </div>
+                <div className="flex flex-col gap-2">
+                    <FormInput {...register("email")} placeholder="Email" name="email" type={"email"}/>
+                    {errors.email && <p className="text-[13px] text-red-500 font-[600]">{errors.email.message}</p>}
+                </div>
+                <div className="flex flex-col gap-2">
+                    <PasswordInput {...register("password")} placeholder="Password" name="password" type={"password"}/>
+                    {errors.password && <p className="text-[13px] text-red-500 font-[600]">{errors.password.message}</p>}
+                </div>
+                <ButtonPrimary
+                    disabled={isSubmitting}
+                    variant={"primary"}
+                >
+                    {isSubmitting ? "Signing up" : "Sign up"}
                 </ButtonPrimary>
                 <div className="text-[var(--muted)] text-[14px] text-center">
                     By signing up, you agree with our <Link className="underline" href="#">Terms & Conditions</Link>
