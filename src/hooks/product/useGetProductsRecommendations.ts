c133e620aac1f95370e4c3cc3a1451c148cdc571
kukarachass <maksimvozniy18@gmail.com>
2026-06-29 22:35:13 +0200

    Update useGetProductsRecommendations.ts

--- src/hooks/product/useGetProductsRecommendations.ts
+++ src/hooks/product/useGetProductsRecommendations.ts
@@ -1,6 +1,5 @@
 import {useQuery} from "@tanstack/react-query";
 import productsRecommendations from "@/components/product/productsRecommendations";
-import {ProductWithDetails} from "@/types/product-details";
 import {CartItemWithDetails} from "@/types/cart";
 
 
