69e945a13450346f3cbe43da9f3793012e8733fc
kukarachass <maksimvozniy18@gmail.com>
2026-07-04 23:56:47 +0200

    Update SocialsMethod.tsx

--- src/components/auth/SocialsMethod.tsx
+++ src/components/auth/SocialsMethod.tsx
@@ -16,14 +16,14 @@ export default function SocialsMethod() {
         <div className="flex flex-row gap-4 items-center w-full">
             <button onClick={() => handleSocialsSignIn("google")}
                 className="group flex flex-row gap-2 items-center justify-center bg-black/4 rounded-[10px] cursor-pointer w-full py-2 px-8 transition-all duration-300 hover:bg-black hover:shadow-[0_4px_20px_rgba(0,0,0,0.15)]">
-                <Image src={"auth/google.svg"} alt={"google"} width={20} height={20}
+                <Image src={"/auth/google.svg"} alt={"google"} width={20} height={20}
                        className="transition-all duration-300 group-hover:invert"/>
                 <span
                     className="text-[var(--text)] text-[16px] font-[600] transition-colors duration-300 group-hover:text-white">Google</span>
             </button>
             <button onClick={() => handleSocialsSignIn("facebook")}
                 className="group flex flex-row gap-2 items-center justify-center bg-black/4 rounded-[10px] cursor-pointer w-full py-2 px-8 transition-all duration-300 hover:bg-black hover:shadow-[0_4px_20px_rgba(0,0,0,0.15)]">
-                <Image src={"auth/facebook.svg"} alt={"facebook"} width={20} height={20}
+                <Image src={"/auth/facebook.svg"} alt={"facebook"} width={20} height={20}
                        className="transition-all duration-300 group-hover:invert"/>
                 <span
                     className="text-[var(--text)] text-[16px] font-[600] transition-colors duration-300 group-hover:text-white">Facebook</span>
