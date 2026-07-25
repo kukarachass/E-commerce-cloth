6e73504c131449c2cef07f3e2cbf50998d8c9052
kukarachass <maksimvozniy18@gmail.com>
2026-07-25 22:52:42 +0200

    useadmin hook was deleted for sec

--- src/hooks/useIsAdmin.ts
+++
@@ -1,9 +0,0 @@
-import {useQuery} from "@tanstack/react-query";
-import {isAdmin} from "@/lib/admin/rbac";
-
-export default function useIsAdmin(){
-    return useQuery({
-        queryKey: ["admin"],
-        queryFn: () => isAdmin()
-    })
-}
