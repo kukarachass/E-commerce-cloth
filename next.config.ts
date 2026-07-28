a984969307ba51608932146e5b60e118f5d863d6
kukarachass <maksimvozniy18@gmail.com>
2026-07-28 21:21:22 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
-        serverActions: { bodySizeLimit: "8mb" },
+  experimental: {
+    serverActions: { bodySizeLimit: "8mb" },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
       },
-      images: {
-        remotePatterns: [
-          { protocol: "https", hostname: "*.supabase.co", pathname: "/storage/v1/object/public/**" },
-        ],
+      {
+        protocol: "https",
+        hostname: "**",
       },
+    ],
+  },
 };
 
 export default nextConfig;
-
